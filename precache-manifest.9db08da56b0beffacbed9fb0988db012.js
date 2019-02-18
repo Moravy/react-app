b33e1191d401b252bd8193bbea6a5d4836725a31
Moravy <moravy22@gmail.com>
2019-02-18 16:11:47 +1300

    Updates

---
+++ precache-manifest.9db08da56b0beffacbed9fb0988db012.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "50e0632ef0d7ad7ff8910dc39d56c7ec",
+    "url": "/react-app/mains.jsx/static/media/mainlogo.50e0632e.png"
+  },
+  {
+    "revision": "2bcdac6b9c4f70256b36",
+    "url": "/react-app/mains.jsx/static/css/main.25f90941.chunk.css"
+  },
+  {
+    "revision": "3553d681ebc52c127ad1",
+    "url": "/react-app/mains.jsx/static/js/runtime~main.3553d681.js"
+  },
+  {
+    "revision": "8355bbb4ff13c08c5ef8c2441e7779d9",
+    "url": "/react-app/mains.jsx/static/media/bw4.8355bbb4.jpg"
+  },
+  {
+    "revision": "4f4434d84ad532d5498a",
+    "url": "/react-app/mains.jsx/static/js/2.4f4434d8.chunk.js"
+  },
+  {
+    "revision": "2bcdac6b9c4f70256b36",
+    "url": "/react-app/mains.jsx/static/js/main.2bcdac6b.chunk.js"
+  },
+  {
+    "revision": "5515f91ed7b96d87438f1965b147b9ea",
+    "url": "/react-app/mains.jsx/static/media/aws.5515f91e.png"
+  },
+  {
+    "revision": "7b487cfcc192ccf25535747047b45286",
+    "url": "/react-app/mains.jsx/static/media/website.7b487cfc.PNG"
+  },
+  {
+    "revision": "0a36ec9dff43940023bd4c11a27b0976",
+    "url": "/react-app/mains.jsx/static/media/MoravyOum.0a36ec9d.pdf"
+  },
+  {
+    "revision": "4f4434d84ad532d5498a",
+    "url": "/react-app/mains.jsx/static/css/2.944121ce.chunk.css"
+  },
+  {
+    "revision": "34026131ed5e019434918d50b3754c5b",
+    "url": "/react-app/mains.jsx/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-app/precache-manifest.64b279e41d7ebfbf4907da800bc92318.js"
+  "/react-app/mains.jsx/precache-manifest.9db08da56b0beffacbed9fb0988db012.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-app/index.html", {
+workbox.routing.registerNavigationRoute("/react-app/mains.jsx/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
