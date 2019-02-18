cd6f88f3b0113ea10770202df2b2e1430097c051
Moravy <moravy22@gmail.com>
2019-02-18 15:14:51 +1300

    Updates

---
+++ precache-manifest.64b279e41d7ebfbf4907da800bc92318.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8355bbb4ff13c08c5ef8c2441e7779d9",
+    "url": "/react-app/static/media/bw4.8355bbb4.jpg"
+  },
+  {
+    "revision": "8ce05037bfd6015f067a",
+    "url": "/react-app/static/js/runtime~main.8ce05037.js"
+  },
+  {
+    "revision": "2d7bafc2a0993a8e4dd0",
+    "url": "/react-app/static/js/main.2d7bafc2.chunk.js"
+  },
+  {
+    "revision": "4de3ce5a034a8b525f63",
+    "url": "/react-app/static/js/2.4de3ce5a.chunk.js"
+  },
+  {
+    "revision": "2d7bafc2a0993a8e4dd0",
+    "url": "/react-app/static/css/main.24f08ed0.chunk.css"
+  },
+  {
+    "revision": "4de3ce5a034a8b525f63",
+    "url": "/react-app/static/css/2.944121ce.chunk.css"
+  },
+  {
+    "revision": "140210ce7575d15f7476dd0c12d854ac",
+    "url": "/react-app/index.html"
+  }
+];
